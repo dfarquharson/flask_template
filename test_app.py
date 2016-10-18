995f1afefd95537e1de6214b3782c6ef25291566
David Farquharson <djfarquharson@spscommerce.com>
2016-10-18 15:02:06 -0500

    test pep8 compliance always.

--- test_app.py
+++ test_app.py
@@ -1,16 +1,17 @@
 import requests
+import pep8
+import os
 
 
 def test_universe():
     assert True
 
 
-def test_another():
-    assert 1 + 1 == 2
-
-
-def test_2():
-    assert 1 == 1
+def test_pep8_compliance():
+    assert len(pep8.StyleGuide()
+               .check_files(filter(lambda x: x.endswith('.py'),
+                            os.listdir('.')))
+               .get_statistics()) == 0
 
 
 def test_up():
